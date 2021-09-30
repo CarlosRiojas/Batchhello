065ca7dc4bb1553e0413320823d1fa9d17b9ddb2
CarlosRiojas <carlos.riojas88@gmail.com>
2021-09-29 23:32:34 -0500

    Stuck on loadController

--- src/main/java/com/BatchHello/BatchExample/config/SpringBatchConfig.java
+++ src/main/java/com/BatchHello/BatchExample/config/SpringBatchConfig.java
@@ -1,15 +1,83 @@
 package com.BatchHello.BatchExample.config;
 
+import com.BatchHello.BatchExample.model.User;
+import jdk.dynalink.linker.support.DefaultInternalObjectFilter;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.LineMapper;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 
 @Configuration
 public class SpringBatchConfig {
+
     @Bean
-    public Job job(JobBuilderFactory jobBuilderFactory){
-        jobBuilderFactory.get("ETL-Load")
+    public Job job(JobBuilderFactory jobBuilderFactory,
+                   StepBuilderFactory stepBuilderFactory,
+                   ItemReader<User> itemReader,
+                   ItemProcessor<User,User> itemProcessor,
+                   ItemWriter<User> itemWriter) {
+
+        Step step = stepBuilderFactory.get("ETL-file-load")
+                .chunk(100)
+                .reader(itemReader)
+                .processor(itemProcessor)
+                .writer(itemWriter)
+                .build();
+
+        Job job = jobBuilderFactory.get("ETL-Load")
+                .incrementer(new RunIdIncrementer())
+                .start(step)
+                .build();
+        return job;
+        /*
+        * if  we had the need to start another consecutive step
+        * we could use .flow()
+        *              .next()
+        *              .build()
+        * */
     }
+
+    @Bean
+    public FlatFileItemReader<User> itemReader(@Value("${input}") Resource resource){
+        FlatFileItemReader<User> flatFileItemReader = new FlatFileItemReader<>();
+        flatFileItemReader.setResource(resource);
+        flatFileItemReader.setName("CSV Reader");
+        flatFileItemReader.setLinesToSkip(1);
+        flatFileItemReader.setLineMapper(lineMapper());
+        return flatFileItemReader;
+    }
+
+    @Bean
+    private LineMapper<User> lineMapper() {
+        DefaultLineMapper<User> defaultLineMapper = new DefaultLineMapper<>();
+        DelimitedLineTokenizer lineTokenizer = new DelimitedLineTokenizer();
+
+        lineTokenizer.setDelimiter(",");
+        lineTokenizer.setStrict(false);
+        lineTokenizer.setNames(new String[]{"id", "name", "dept", "salary"});
+
+        BeanWrapperFieldSetMapper<User> fieldSetMapper = new BeanWrapperFieldSetMapper<>();
+        fieldSetMapper.setTargetType(User.class);
+
+        defaultLineMapper.setLineTokenizer(lineTokenizer);
+        defaultLineMapper.setFieldSetMapper(fieldSetMapper);
+
+        return null;
+    }
+
 }
